efc2d86b92b6a10b6418b9cdc81e9411beff4337
huxiaoou <516984451@qq.com>
2021-11-15 15:23:41 +0800

    Init commit.

---
+++ mean_variance_alg.py
@@ -0,0 +1,373 @@
+import numpy as np
+import scipy.optimize
+from scipy.optimize import minimize
+
+"""
+created @ 2021-06-16
+0.  provide algorithm and functions to solve assets allocation problem
+updated @ 2021-09-19
+"""
+
+
+def portfolio_variance(t_w: np.ndarray, t_sigma: np.ndarray) -> float:
+    return t_w.dot(t_sigma).dot(t_w)
+
+
+def jac_var(t_w: np.ndarray, t_sigma: np.ndarray) -> np.ndarray:
+    return 2 * t_sigma.dot(t_w)
+
+
+def portfolio_std(t_w: np.ndarray, t_sigma: np.ndarray) -> float:
+    return np.sqrt(t_w.dot(t_sigma).dot(t_w))
+
+
+def portfolio_return(t_w: np.ndarray, t_mu: np.ndarray) -> float:
+    return t_w.dot(t_mu)
+
+
+def jac_ret(t_w: np.ndarray, t_mu: np.ndarray) -> np.ndarray:  # d(kx) / dx = k, constant k is irrelevant to the x
+    return t_mu
+
+
+def portfolio_return_mirror(t_w: np.ndarray, t_mu: np.ndarray) -> float:
+    return -t_w.dot(t_mu)
+
+
+def portfolio_utility(t_w: np.ndarray, t_mu: np.ndarray, t_sigma: np.ndarray, t_lbd: float) -> float:
+    # u = -2 w*m/l +wSw  <=> v = w*m - l/2 * wSw
+    return -2 * portfolio_return(t_w, t_mu) / t_lbd + portfolio_variance(t_w, t_sigma)
+
+
+def portfolio_risk_budget(t_w: np.ndarray, t_sigma: np.ndarray, t_rb: np.ndarray, t_verbose: bool) -> float:
+    _p, _ = t_sigma.shape
+    _s = np.sqrt(portfolio_variance(t_w, t_sigma))
+    _mrc = t_sigma.dot(t_w) / _s  # marginal risk cost
+    _rc = t_w * _mrc  # risk cost
+    _x = _rc / t_rb
+    if t_verbose:
+        print("RB  = {}".format(t_rb))
+        print("MRC = {}".format(_mrc))
+        print("RC  = {}".format(_rc))
+        print("X   = {}".format(_x))
+    return 2 * (_p ** 2) * np.var(_x, ddof=0)
+
+
+# -------------------------------- Optimize Algorithm --------------------------------
+# --- type 0: minimum variance
+# target: min_{w} wSw  with w1 = 1
+def minimize_variance(t_sigma: np.ndarray) -> (np.ndarray, float):
+    """
+    :param t_sigma: the covariance of available assets
+    :return:
+    """
+    _p, _ = t_sigma.shape
+    _u = np.ones(_p)
+    _h = np.linalg.inv(t_sigma).dot(_u)
+    _w = _h / (_u.dot(_h))
+    _min_var = portfolio_variance(t_w=_w, t_sigma=t_sigma)
+    return _w, _min_var
+
+
+def minimize_variance_con(t_sigma: np.ndarray) -> (np.ndarray, float):
+    """
+    :param t_sigma: the covariance of available assets
+    :return: 
+    """
+    _p, _ = t_sigma.shape
+    _res = minimize(
+        fun=portfolio_variance,
+        x0=np.ones(_p) / _p,
+        args=(t_sigma,),
+        bounds=[(0, 1)] * _p,
+        constraints={"type": "eq", "fun": lambda z: z.sum() - 1}
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
+
+
+def minimize_variance_con2(t_sigma: np.ndarray) -> (np.ndarray, float):
+    """
+    :param t_sigma: the covariance of available assets
+    :return:
+    """
+    _p, _ = t_sigma.shape
+    _res = minimize(
+        fun=portfolio_variance,
+        x0=np.ones(_p) / _p,
+        args=(t_sigma,),
+        bounds=[(-1, 1)] * _p,  # allow short
+        constraints={"type": "eq", "fun": lambda z: z.sum() - 1},
+        # constraints=scipy.optimize.LinearConstraint(np.ones(_p), -1, 1), # can not be, or the result would be ZERO vector
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
+
+
+# --- type 1: minimum utility
+# target: min_{w} -2/lambda * wm + wSw  with w1 = 1
+def minimize_utility(t_mu: np.ndarray, t_sigma: np.ndarray, t_lbd) -> (np.ndarray, float):
+    _p, _ = t_sigma.shape
+    _u = np.ones(_p)
+    _sigma_inv = np.linalg.inv(t_sigma)
+    _m = _sigma_inv.dot(t_mu)
+    _h = _sigma_inv.dot(_u)
+    _d = 2 * (1 - 1 / t_lbd * _u.dot(_m)) / (_u.dot(_h))
+    _w = 1 / t_lbd * _m + _d / 2 * _h
+    _uty = portfolio_utility(_w, t_mu, t_sigma, t_lbd)
+    return _w, _uty
+
+
+def minimize_utility_con(t_mu: np.ndarray, t_sigma: np.ndarray, t_lbd: float, t_bounds: tuple = (0, 1)) -> (np.ndarray, float):
+    _p, _ = t_sigma.shape
+    _res = minimize(
+        fun=portfolio_utility,
+        x0=np.ones(_p) / _p,
+        args=(t_mu, t_sigma, t_lbd),
+        bounds=[t_bounds] * _p,
+        constraints={"type": "eq", "fun": lambda z: z.sum() - 1}
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
+
+
+def minimize_utility_con2(t_mu: np.ndarray, t_sigma: np.ndarray, t_lbd: float) -> (np.ndarray, float):
+    _p, _ = t_sigma.shape
+    _res = minimize(
+        fun=portfolio_utility,
+        x0=np.ones(_p) / _p,
+        args=(t_mu, t_sigma, t_lbd),
+        bounds=[(-1, 1)] * _p,  # allow short
+        constraints=scipy.optimize.LinearConstraint(np.ones(_p), -1, 1),  # control total market value
+
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
+
+
+def minimize_utility_con3(t_mu: np.ndarray, t_sigma: np.ndarray, t_lbd: float,
+                          t_bound: float, t_max_iter: int = 50000) -> (np.ndarray, float):
+    _p, _ = t_sigma.shape
+    _res = minimize(
+        fun=portfolio_utility,
+        x0=np.ones(_p) / _p,
+        args=(t_mu, t_sigma, t_lbd),
+        bounds=[(-t_bound, t_bound)] * _p,  # allow short
+        constraints=scipy.optimize.NonlinearConstraint(lambda z: np.sum(np.abs(z)), 0, 1),  # control total market value
+        options={"maxiter": t_max_iter}
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
+
+
+def minimize_utility_con4(t_mu: np.ndarray, t_sigma: np.ndarray, t_lbd: float,
+                          t_sec: np.ndarray, t_sec_l_bound: np.ndarray, t_sec_r_bound: np.ndarray, t_max_iter: int = 50000) -> (np.ndarray, float):
+    _p, _ = t_sigma.shape
+    _a = np.vstack([np.ones(_p), t_sec])
+    _lb = np.concatenate(([0], t_sec_l_bound))
+    _rb = np.concatenate(([1], t_sec_r_bound))
+    _res = minimize(
+        fun=portfolio_utility,
+        x0=np.ones(_p) / _p,
+        args=(t_mu, t_sigma, t_lbd),
+        constraints=scipy.optimize.NonlinearConstraint(lambda z: _a.dot(np.abs(z)), _lb, _rb),  # control total market value
+        options={"maxiter": t_max_iter}
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
+
+
+def minimize_utility_con5(t_mu: np.ndarray, t_sigma: np.ndarray, t_lbd: float,
+                          t_bound, t_sec: np.ndarray, t_sec_l_bound: np.ndarray, t_sec_r_bound: np.ndarray, t_max_iter: int = 50000) -> (np.ndarray, float):
+    _p, _ = t_sigma.shape
+    _res = minimize(
+        fun=portfolio_utility,
+        x0=np.ones(_p) / _p,
+        args=(t_mu, t_sigma, t_lbd),
+        bounds=[(-t_bound, t_bound)] * _p,  # allow short
+        constraints=[
+            scipy.optimize.NonlinearConstraint(lambda z: np.sum(np.abs(z)), 0, 1),
+            scipy.optimize.LinearConstraint(t_sec, t_sec_l_bound, t_sec_r_bound),
+        ],  # control total market value
+        options={"maxiter": t_max_iter}
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
+
+
+def minimize_utility_con_analytic(t_mu: np.ndarray, t_sigma: np.ndarray, t_lbd: float, t_H: np.ndarray, t_h: np.ndarray, t_F: np.ndarray, t_f: np.ndarray) -> (np.ndarray, float):
+    """
+
+    :param t_mu: the mean of assets return, shape: P x 1
+    :param t_sigma: the covariance of available assets, shape: P x P
+    :param t_lbd: risk aversion coefficient, shape: 1 x 1
+    :param t_H: equality confine left, shape: M x P, M = number of equality confine
+    :param t_h: equality confine right, shape: M x 1
+    :param t_F: inequality confine left, shape: N x P, N = number of inequality confine
+    :param t_f: inequality confine right, shape: N x 1,
+    :return: the optimal w'm- 2w'Sw/l with Hw=h, Fw <=f
+    """
+
+    """
+    this function has some problems, can NOT be used yet.
+    """
+    _sigma_inv = np.linalg.inv(t_sigma)  # shape: p x p
+
+    _z00 = t_H.dot(_sigma_inv).dot(t_H.T)  # shape: M x M
+    _z01 = t_H.dot(_sigma_inv).dot(t_F.T)  # shape: M x N
+    _z10 = t_F.dot(_sigma_inv).dot(t_H.T)  # shape: N x M
+    _z11 = t_F.dot(_sigma_inv).dot(t_F.T)  # shape: N x N
+    _z = np.concatenate([
+        np.concatenate([_z00, _z01], axis=1),
+        np.concatenate([_z10, _z11], axis=1),
+    ], axis=0)  # shape: (M+N) x (M+N)
+
+    _k0 = t_lbd * t_h - t_H.dot(_sigma_inv).dot(t_mu)  # shape: M x 1
+    _k1 = t_lbd * t_f - t_F.dot(_sigma_inv).dot(t_mu)  # shape: N x 1
+    _k = np.concatenate([_k0, _k1])  # shape: (M+N) x 1
+
+    _z_inv = np.linalg.inv(_z)  # shape: (M+N) x (M+N)
+    _alpha_beta = _z_inv.dot(_k)  # shape: (M+N) x 1
+    _H_F = np.array([t_H.T, t_F.T])
+    _w = _sigma_inv.dot(t_mu) / t_lbd + _sigma_inv.dot(_H_F).dot(_z_inv).dot(_alpha_beta) / t_lbd
+    _uty = portfolio_utility(_w, t_mu, t_sigma, t_lbd)
+    return _w, _uty
+
+
+# --- type 2: risk parity/budget
+# target: min_{w} sum_{i}(w_i(sigma*w)_i/s - s/n)
+def minimize_risk_budget_con(t_sigma: np.ndarray, t_rb: np.ndarray, t_verbose) -> (np.ndarray, float):
+    """
+    :param t_sigma: the covariance of available assets
+    :param t_rb: risk budget
+    :param t_verbose: whether to print iteration details
+    :return:
+    """
+    _p, _ = t_sigma.shape
+    _res = minimize(
+        fun=portfolio_risk_budget,
+        x0=np.ones(_p) / _p,
+        args=(t_sigma, t_rb, t_verbose),
+        bounds=[(0, 1)] * _p,
+        constraints={"type": "eq", "fun": lambda z: z.sum() - 1},
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
+
+
+def minimize_risk_budget_con2(t_sigma: np.ndarray, t_rb: np.ndarray, t_verbose) -> (np.ndarray, float):
+    """
+    :param t_sigma: the covariance of available assets
+    :param t_rb: risk budget
+    :param t_verbose: whether to print iteration details
+    :return:
+    """
+    _p, _ = t_sigma.shape
+    _res = minimize(
+        fun=portfolio_risk_budget,
+        x0=np.ones(_p) / _p,
+        args=(t_sigma, t_rb, t_verbose),
+        bounds=[(-1, 1)] * _p,  # allow short
+        constraints=scipy.optimize.LinearConstraint(np.ones(_p), -1, 1),  # control total market value
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
+
+
+# --- type 3: maximum return with confined variance
+# may have convergence problem, can not be used yet.
+def maximum_return_with_confined_variance(t_mu: np.ndarray, t_sigma: np.ndarray, t_benchmark_s2: float):
+    """
+
+    :param t_mu: the expected return of  available assets
+    :param t_sigma: the covariance of available assets
+    :param t_benchmark_s2: the expected variance of the portfolio
+    :return:
+    """
+    _p, _ = t_sigma.shape
+    _res = minimize(
+        fun=portfolio_return_mirror,
+        x0=np.ones(_p) / _p,
+        args=(t_mu,),
+        method="SLSQP",
+        jac=jac_ret,
+        bounds=[(0, 1)] * _p,
+        constraints=[
+            {"type": "eq", "fun": lambda z: z.sum() - 1},
+            {"type": "eq", "fun": lambda z: portfolio_variance(z, t_sigma) - t_benchmark_s2},
+        ],
+        options={"maxiter": 1000, "ftol": 0.1}
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
+
+
+# --- type 4: minimum variance with confined return
+# may have convergence problem, can not be used yet.
+def minimum_variance_with_confined_return(t_mu: np.ndarray, t_sigma: np.ndarray, t_benchmark_mu: float):
+    """
+
+    :param t_mu: the expected return of  available assets
+    :param t_sigma: the covariance of available assets
+    :param t_benchmark_mu: the expected return of the portfolio
+    :return:
+    """
+    _p, _ = t_sigma.shape
+    _res = minimize(
+        fun=portfolio_variance,
+        x0=np.ones(_p) / _p,
+        args=(t_sigma,),
+        method="SLSQP",
+        jac=jac_var,
+        bounds=[(0, 1)] * _p,
+        constraints=[
+            {"type": "eq", "fun": lambda z: z.sum() - 1},
+            {"type": "eq", "fun": lambda z: portfolio_return(z, t_mu) - t_benchmark_mu},
+        ],
+        options={"maxiter": 1000, "ftol": 0.1}
+    )
+    if _res.success:
+        return _res.x, _res.fun
+    else:
+        print("ERROR! Optimizer exits with a failure")
+        print("Detailed Description: {}".format(_res.message))
+        return None, None
---
+++ unit_test.py
@@ -0,0 +1,104 @@
+import numpy as np
+from mean_variance_alg import *
+
+np.set_printoptions(4, suppress=True)
+
+mu = np.array([0.8, -0.2, 0.3])
+sigma = np.array([
+    [1, 0.5, -0.3],
+    [0.5, 2, -0.1],
+    [-0.3, -0.1, 3],
+])
+lbd = 1
+confined_variance = 1
+confined_return = 0.5
+H = np.array([
+    np.ones(len(mu))
+])
+h = np.array([
+    1
+])
+F = np.concatenate([
+    np.diag(np.ones(len(mu))),
+    -np.diag(np.ones(len(mu))),
+], axis=0)
+f = np.concatenate([
+    np.ones(len(mu)),
+    np.zeros(len(mu)),
+], axis=0)
+
+# # -------------------------------------------------------------
+# print("\nminimize_variance")
+# w, var_min = minimize_variance(t_sigma=sigma)
+# print("w       = {}".format(w))
+# print("var_min = {:.6f}".format(var_min))
+#
+# print("\nminimize_variance_con")
+# w, var_min = minimize_variance_con(t_sigma=sigma)
+# print("w       = {}".format(w))
+# print("var_min = {:.6f}".format(var_min))
+
+# -------------------------------------------------------------
+print("\nminimize_utility")
+w, utility_min = minimize_utility(t_mu=mu, t_sigma=sigma, t_lbd=lbd)
+print("w           = {}".format(w))
+print("utility_min = {:.6f}".format(utility_min))
+
+print("\nminimize_utility_con")
+w, utility_min = minimize_utility_con(t_mu=mu, t_sigma=sigma, t_lbd=lbd)
+print("w           = {}".format(w))
+print("utility_min = {:.6f}".format(utility_min))
+
+print("\nminimize_utility_con_analytic")
+w, utility_min = minimize_utility_con_analytic(t_mu=mu, t_sigma=sigma, t_lbd=lbd, t_H=H, t_h=h, t_F=F, t_f=f)
+print("w           = {}".format(w))
+print("utility_min = {:.6f}".format(utility_min))
+
+# print("\nminimize_utility_con2")
+# w, utility_min = minimize_utility_con2(t_mu=mu, t_sigma=sigma, t_lbd=lbd)
+# print("w           = {}".format(w))
+# print("utility_min = {:.6f}".format(utility_min))
+
+# # -------------------------------------------------------------
+# print("\nminimize_risk_budget_con")
+# rb = np.ones(shape=mu.shape)
+# w, trc_min = minimize_risk_budget_con(t_sigma=sigma, t_rb=rb, t_verbose=False)
+# print("w       = {}".format(w))
+# print("TRC     = {:.12f}".format(trc_min))
+# portfolio_risk_budget(t_w=w, t_sigma=sigma, t_rb=rb, t_verbose=True)
+#
+# print("\nminimize_risk_budget_con2")
+# rb = np.ones(shape=mu.shape)
+# w, trc_min = minimize_risk_budget_con2(t_sigma=sigma, t_rb=rb, t_verbose=False)
+# print("w       = {}".format(w))
+# print("TRC     = {:.12f}".format(trc_min))
+# portfolio_risk_budget(t_w=w, t_sigma=sigma, t_rb=rb, t_verbose=True)
+#
+# print("\nminimize_risk_budget_con")
+# rb = np.array([10, 1, 2])
+# w, trc_min = minimize_risk_budget_con(t_sigma=sigma, t_rb=rb, t_verbose=False)
+# print("w       = {}".format(w))
+# print("TRC     = {:.12f}".format(trc_min))
+# portfolio_risk_budget(t_w=w, t_sigma=sigma, t_rb=rb, t_verbose=True)
+#
+# print("\nminimize_risk_budget_con2")
+# rb = np.array([10, 1, 2])
+# w, trc_min = minimize_risk_budget_con2(t_sigma=sigma, t_rb=rb, t_verbose=False)
+# print("w       = {}".format(w))
+# print("TRC     = {:.12f}".format(trc_min))
+# portfolio_risk_budget(t_w=w, t_sigma=sigma, t_rb=rb, t_verbose=True)
+#
+# # -------------------------------------------------------------
+# print("\nmaximum return with confined variance")
+# w, maximum_return = maximum_return_with_confined_variance(t_mu=mu, t_sigma=sigma, t_benchmark_s2=confined_variance)
+# if w is not None:
+#     print("w       = {}".format(w))
+#     print("confined variance = {:.6f}".format(confined_variance))
+#     print("maximum  return   = {:.6f}".format(-maximum_return))
+#
+# print("\nminimum variance with confined return")
+# w, minimum_variance = minimum_variance_with_confined_return(t_mu=mu, t_sigma=sigma, t_benchmark_mu=confined_return)
+# if w is not None:
+#     print("w       = {}".format(w))
+#     print("minimum variance  = {:.6f}".format(minimum_variance))
+#     print("confined return   = {:.6f}".format(confined_return))
